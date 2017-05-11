a13a4814def1733f250d8ef6e1ef9368d3e16d2a
Kamil <blueflamesxcode@gmail.com>
2017-05-11 13:33:52 +0200

    Fasola merguj to!

--- game_core.py
+++ game_core.py
@@ -61,6 +61,7 @@ def load_footballer(position, tier):
 
     return loaded_footballer
 
+
 def getch():
     import sys, tty, termios
     fd = sys.stdin.fileno()
@@ -82,7 +83,7 @@ def create_first_board(width, height, sign):
             if row == 0 or row == (height - 1):
                 item_in_row.append("#")
             else:
-                if column == 0 or column == 150 or column == 178:
+                if column == 0 or column == 150 or column == 198:
                     item_in_row.append("#")
                 else:
                     item_in_row.append(sign)
@@ -91,6 +92,15 @@ def create_first_board(width, height, sign):
     return board
 
 
+def insert_items_from_backpack(backpack, inventory_board, pos_x=5, pos_y=5):
+
+    for y in range(len(backpack)):
+        record = backpack[y][0]+" "+backpack[y][1]+" "+backpack[y][2]+" "+str(backpack[y][3])
+        for x in range(len(record)):
+            inventory_board[y+pos_y][x+pos_x] = record[x]
+
+    return inventory_board
+
 def create_inventory_board(width, height):
     inventory_board = []
 
@@ -98,11 +108,114 @@ def create_inventory_board(width, height):
         temporary_storage = []
 
         for column in range(width):
-            temporary_storage.append(".")
+            if row == 0 or row == (height - 1) or row == 6:
+                temporary_storage.append("_")
+            else:
+                if column == 0 or column == (width - 1) or column == int(width/2):
+                    temporary_storage.append("|")
+                else:
+                    temporary_storage.append(" ")
 
         inventory_board.append(temporary_storage)
 
     return inventory_board
+
+
+def highest_length_of_item_inside_list(stuff):
+
+    longest_chain_of_items = 0
+
+    for items_list in stuff:
+        temporary_value = 0
+
+        for item in items_list:
+            temporary_value += len(str(item))
+
+        if temporary_value > longest_chain_of_items:
+            longest_chain_of_items = temporary_value
+
+    return longest_chain_of_items
+
+def find_longest_item(*data_space):
+    data_space = list(data_space)
+    longest = 0
+
+    for item in data_space:
+        if highest_length_of_item_inside_list(item) > longest:
+            longest = highest_length_of_item_inside_list(item)
+
+    return longest
+
+
+def handle_backpack(main_footballers_list, sub_footballers_list):
+
+    longest_item = find_longest_item(main_footballers_list, sub_footballers_list)
+
+    backpack_height = int((len(main_footballers_list) + len(sub_footballers_list)) * 1.8) + 10
+    backpack_width = longest_item * 4
+
+    inventory_board = create_inventory_board(backpack_width, backpack_height)
+
+    middle_point = int((backpack_width) / 2)
+    space_between_backpacks = int((middle_point - longest_item) / 2)
+    main_backpack = space_between_backpacks
+    minor_backpack = middle_point+space_between_backpacks
+
+    insert_items_from_backpack(main_footballers_list, inventory_board, main_backpack, 8)
+    insert_items_from_backpack(sub_footballers_list, inventory_board, minor_backpack, 8)
+
+    return inventory_board, main_footballers_list, sub_footballers_list, middle_point
+
+
+def insert_stressed_line(inventory_board, vertical, horizontal ):
+    inventory_board[vertical][horizontal] = "A"
+
+    return inventory_board
+
+
+def stress_line_up(pressed_key, vertical, horizontal, length_main, length_sub, middle_point):
+
+    if horizontal == 1:
+        if pressed_key == "8" and vertical > 8:
+            vertical -= 1
+        elif pressed_key == "7" and vertical < 7 + length_main:
+            vertical += 1
+
+    if horizontal == (middle_point+1):
+        if pressed_key == "8" and vertical > 8:
+            vertical -= 1
+        elif pressed_key == "7" and vertical < 7 + length_sub:
+            vertical += 1
+
+    return vertical, horizontal
+
+
+def exchange_items_between_backpacks(vertical, horizontal, middle_point, main_list, sub_list):
+    if horizontal == 1:
+        if main_list:
+            footballer = main_list.pop(vertical - 8)
+            sub_list.append(footballer)
+
+    elif horizontal == (middle_point + 1):
+        if sub_list:
+            footballer = sub_list.pop(vertical - 8)
+            main_list.append(footballer)
+
+    if vertical > 8:
+        vertical -= 1
+
+    return vertical, main_list, sub_list
+def input_and_refresh_backpacks(pressed_key, counter):
+
+    if counter % 2 == 0:    # counter to pass two runs at once by the main inventory loop
+        pressed_key = getch()
+    else:
+        pressed_key = None
+    counter += 1
+    os.system('clear')
+
+    return pressed_key, counter
+
 
 def create_building(box_range, board, pos_x=0, pos_y=0):
 
@@ -117,6 +230,53 @@ def create_building(box_range, board, pos_x=0, pos_y=0):
                     board[x + pos_x][y + pos_y] = '\033[30m' + '.' + '\33[37m'
     return board
 
+def inventory_main_view(main_footballers_list, sub_footballers_list, pressed_key):
+    stressed_line_horizontal_pos = 1
+    stressed_line_vertical_pos = 8
+    counter = 2
+    inventory_opened = True
+
+    if main_footballers_list or sub_footballers_list:
+        while inventory_opened:
+
+            (inventory_board,
+            main_footballers_list,
+            sub_footballers_list,
+            middle_point) = handle_backpack(main_footballers_list, sub_footballers_list)
+
+            pressed_key, counter = input_and_refresh_backpacks(pressed_key, counter) # pass loop one more time at once
+
+            if pressed_key in ["7", "8"]:  # move through backpack
+
+                stressed_line_vertical_pos, stressed_line_horizontal_pos = stress_line_up(pressed_key,
+                                                                            stressed_line_vertical_pos,
+                                                                            stressed_line_horizontal_pos,
+                                                                            len(main_footballers_list),
+                                                                            len(sub_footballers_list),
+                                                                            middle_point)
+            elif pressed_key == "]":     # Go Through sub_backpack
+                stressed_line_horizontal_pos = middle_point+1
+
+            elif pressed_key == "[":     # Go through main_backpack
+                stressed_line_horizontal_pos = 1
+
+            elif pressed_key == "5":  # move items between backpacks
+                (stressed_line_vertical_pos,
+                 main_footballers_list,
+                  sub_footballers_list) = exchange_items_between_backpacks(
+                                            stressed_line_vertical_pos,
+                                            stressed_line_horizontal_pos,
+                                            middle_point, main_footballers_list,
+                                            sub_footballers_list)
+            elif pressed_key == 'x':
+                exit()
+
+            insert_stressed_line(inventory_board, stressed_line_vertical_pos, stressed_line_horizontal_pos)
+            print_board(inventory_board)
+
+            if pressed_key == 'b':  # Get out of the backpacks to game view
+                inventory_opened = False
+                os.system('clear')
 
 def create_random_amount_of_buildings(board):
     for buildings in range(501):
@@ -187,6 +347,8 @@ def insert_player(board, pos_x, pos_y, old_h, old_v ):
     board[old_v][old_h] = " "
     board[pos_y][pos_x] = '@'
     return board
+
+
 
 def show_welcome():
 
@@ -289,12 +451,21 @@ def load_data(filename):
     return content
 
 
-def ask_question(difficulty, position, easy_questions, medium_questions, hard_questions, easy_answers, medium_answers, hard_answers, reserve_players):
+def ask_question(difficulty, position,   reserve_players):
 
         """
         difficulty: easy, medium or hard
         """
+
+
+        easy_questions = load_data('easy_questions.csv')
+        medium_questions = load_data('medium_questions.csv')
+        hard_questions = load_data('hard_questions.csv')
+        easy_answers = load_data('easy_answers.csv')
+        medium_answers = load_data('medium_answers.csv')
+        hard_answers = load_data('hard_answers.csv')
         if difficulty == 'easy':
+
             random_digit = randint(0,len(easy_questions) - 1)
             print("If you answer this question, you get random SILVER player!" + "\n" + "If you fail, you will loose precious TIME!" + "\n")
 
@@ -302,7 +473,7 @@ def ask_question(difficulty, position, easy_questions, medium_questions, hard_qu
             answer = input(easy_questions[random_digit])
 
             if answer == easy_answers[random_digit].lower().rstrip():
-                dragon_loot.append("LAZAR")
+
                 reserve_players.append(load_footballer(position, 'SILVER'))
 
 
@@ -329,64 +500,47 @@ def ask_question(difficulty, position, easy_questions, medium_questions, hard_qu
 
 
 
-def generate_question(board,vertical_pos, horizontal_pos, reserve_players):
+def generate_question(board,vertical_pos, horizontal_pos, reserve_players, ):
 
-    easy_questions = load_data('easy_questions.csv')
-    medium_questions = load_data('medium_questions.csv')
-    hard_questions = load_data('hard_questions.csv')
-    easy_answers = load_data('easy_answers.csv')
-    medium_answers = load_data('medium_answers.csv')
-    hard_answers = load_data('hard_answers.csv')
+
 
 
     current_pos = board[vertical_pos][horizontal_pos]
     if current_pos == (colors.SILVER + 'Ⓐ' + colors.END):
-        ask_question('easy', 'att', easy_questions, medium_questions, hard_questions,
-                        easy_answers, medium_answers, hard_answers, reserve_players)
+        ask_question('easy', 'att', reserve_players)
         horizontal_pos += 1
     elif current_pos == (colors.SILVER + 'Ⓜ' + colors.END):
-        ask_question('easy', 'mid', easy_questions, medium_questions, hard_questions,
-                        easy_answers, medium_answers, hard_answers, reserve_players)
+        ask_question('easy', 'mid', reserve_players)
         horizontal_pos += 1
     elif current_pos == (colors.SILVER + 'Ⓓ' + colors.END):
-        ask_question('easy', 'deff', easy_questions, medium_questions, hard_questions,
-                        easy_answers, medium_answers, hard_answers, reserve_players)
+        ask_question('easy', 'deff', reserve_players)
         horizontal_pos += 1
     elif current_pos == (colors.SILVER + 'Ⓖ' + colors.END):
-        ask_question('easy', 'gk', easy_questions, medium_questions, hard_questions,
-                        easy_answers, medium_answers, hard_answers, reserve_players)
+        ask_question('easy', 'gk', reserve_players)
         horizontal_pos += 1
     elif current_pos == (colors.GOLD + 'Ⓐ' + colors.END):
-        ask_question('medium', 'att', easy_questions, medium_questions, hard_questions,
-                        easy_answers, medium_answers, hard_answers, reserve_players)
+        ask_question('medium', 'att', reserve_players)
         horizontal_pos += 1
     elif current_pos == (colors.GOLD + 'Ⓜ' + colors.END):
-        ask_question('medium', 'mid', easy_questions, medium_questions, hard_questions,
-                        easy_answers, medium_answers, hard_answers, reserve_players)
+        ask_question('medium', 'mid', reserve_players)
         horizontal_pos += 1
     elif current_pos == (colors.GOLD + 'Ⓓ' + colors.END):
-        ask_question('medium', 'deff', easy_questions, medium_questions, hard_questions,
-                        easy_answers, medium_answers, hard_answers, reserve_players)
+        ask_question('medium', 'deff', reserve_players)
         horizontal_pos += 1
     elif current_pos == (colors.GOLD + 'Ⓖ' + colors.END):
-        ask_question('medium', 'gk', easy_questions, medium_questions, hard_questions,
-                        easy_answers, medium_answers, hard_answers, reserve_players)
+        ask_question('medium', 'gk', reserve_players)
         horizontal_pos += 1
     elif current_pos == (colors.ELITE + 'Ⓐ' + colors.END):
-        ask_question('hard', 'att', easy_questions, medium_questions, hard_questions,
-                        easy_answers, medium_answers, hard_answers, reserve_players)
+        ask_question('hard', 'att', reserve_players)
         horizontal_pos += 1
     elif current_pos == (colors.ELITE + 'Ⓜ' + colors.END):
-        ask_question('hard', 'mid', easy_questions, medium_questions, hard_questions,
-                        easy_answers, medium_answers, hard_answers, reserve_players)
+        ask_question('hard', 'mid', reserve_players)
         horizontal_pos += 1
     elif current_pos == (colors.ELITE + 'Ⓓ' + colors.END):
-        ask_question('hard', 'deff', easy_questions, medium_questions, hard_questions,
-                        easy_answers, medium_answers, hard_answers, reserve_players)
+        ask_question('hard', 'deff', reserve_players)
         horizontal_pos += 1
     elif current_pos == (colors.ELITE + 'Ⓖ' + colors.END):
-        ask_question('hard', 'gk', easy_questions, medium_questions, hard_questions,
-                        easy_answers, medium_answers, hard_answers, reserve_players)
+        ask_question('hard', 'gk', reserve_players)
 
         horizontal_pos += 1
 
@@ -395,11 +549,12 @@ def generate_question(board,vertical_pos, horizontal_pos, reserve_players):
 
 def main():
 
-    width = 180
+    width = 200
     height = 50
 
 
-    dragon_loot = []
+    main_footballers_list = [["ronaldo", "Tierr", "Pozycja", 86], ["Messi", "Tyr", "PosPos", 92], ["ronaldo", "Tier", "Pozycja", 80], ["ronaldo", "Tier", "Pozycja", 80], ["ronaldo", "Tier", "Pozycja", 80]]
+    sub_footballers_list = [["ronaldo", "Tier", "Pozycja", 89], ["Messi", "Tyr", "PosPos", 92], ["ronaldo", "Tier", "Pozycja", 80], ["ronaldo", "Tier", "Pozycja", 80], ["ronaldo", "Tier", "Pozycja", 80]]
     horizontal_pos  = width//2
     vertical_pos = height//2
 
@@ -456,25 +611,13 @@ def main():
             horizontal_pos, vertical_pos = hero_walking(pressed_key, board, horizontal_pos, vertical_pos)
         elif pressed_key == 'x':
             exit()
+
+
+#-----------------------------------------------------------------------
         elif pressed_key == 'i':
-
-
-            while True:
-                print(dragon_loot)
-                sleep(1)
-                pressed_key = getch()
-                os.system('clear')
-
-                inventory_board = create_inventory_board(101, 101)
-                inventory_board[5][5] = "x"
-                print_board(inventory_board)
-
-
-                if pressed_key == 'x':
-                    exit()
-
-
-        player = insert_player(board, horizontal_pos, vertical_pos, old_horizontal, old_vertical)
+            inventory_main_view(main_footballers_list, sub_footballers_list, pressed_key)
+#---------------------------------------------------------------------------------------
+        insert_player(board, horizontal_pos, vertical_pos, old_horizontal, old_vertical)
 
 
 
